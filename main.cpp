f854b3e8a15d18d5f05d2653ec25e8c407434043
Dmytro Taras <48064427+EGheska@users.noreply.github.com>
2023-02-14 16:34:11 +0100

    bee.txt added

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include <fstream>
+
 
 int main() {
-
+    std::ifstream InputFile;
+    InputFile.open("bee.txt");
+    if (InputFile){
+        std::cout << "1";
+    } else {
+        std::cout << "0";
+    }
     return 0;
 }
