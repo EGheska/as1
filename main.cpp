34db198b830341cee7c9a2501c663bd8e66d7ca0
Dmytro Taras <48064427+EGheska@users.noreply.github.com>
2023-02-14 17:09:51 +0100

    small changes

--- main.cpp
+++ main.cpp
@@ -2,13 +2,24 @@
 #include <fstream>
 
 
+using namespace std;
 int main() {
-    std::ifstream InputFile;
-    InputFile.open("bee.txt");
-    if (InputFile){
-        std::cout << "1";
-    } else {
-        std::cout << "0";
+    fstream files;
+    int userInput[3] = {};
+    for (int i=0;i<3;i++){
+     cin >> userInput[i];
+    }
+    for (int i=0;i<3;i++){
+        if (userInput[i] = 1){
+            files.open("bee.txt");
+            if (files) {
+                cout << "File exist";
+            }
+        } else if (userInput[i] = 2) {
+
+        } else if (userInput[i] = 3){
+
+        }
     }
     return 0;
 }
