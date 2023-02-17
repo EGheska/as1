938e088996cfb1b9c31661c43b3e46bea0383b4f
Dmytro Taras <48064427+EGheska@users.noreply.github.com>
2023-02-17 14:50:21 +0100

    changes

--- main.cpp
+++ main.cpp
@@ -3,46 +3,61 @@
 #include <string>
 
 int main(){
-    std::ifstream file("bee.txt");
+    std::ifstream beeFile("bee.txt");
+    std::ifstream smellyFile("smelly.txt");
     std::string buffer;
-    while(std::getline(file,buffer)){
-        std::cout<<buffer << "\n";
+    std::string choice, choiceReplace;
+    std::cin >> choice >> choiceReplace;
+    if (choice == "bee") {
+        while(std::getline(beeFile,buffer)) {
+            std::cout << buffer << "\n";
+        }
+    } else if (choice == "smelly") {
+        while(std::getline(smellyFile, buffer)){
+            std::cout << buffer << "\n";
+        }
+    } else {
+        std::cerr << "Invalid input";
     }
+    return 0;
 }
 
-//int main1() {
-//    std::ifstream inputFile("bee.txt");
-//
-//    std::ifstream replacedRulesSmelly("smelly_replacement.txt");
-//    std::string choice, choiceReplace;
-//    std::cin >> choice >> choiceReplace;
-//    if (choice == "bee") {
-//        inputFile.open("bee.txt");
-//        if (inputFile.is_open()) {
-//            std::string line;
-//            std::getline(std::cin, line);
-//            std::cout << line << std::endl;
-////            auto replacementRules{ifstream("bee_replacement.txt")};
-////            while (!replacementRules.eof()) {
-////                string line;
-////                getline(replacementRules, line);
-////                cout << line << endl;
-//
-//            std::cout << "File is open\n";
-//        } else {
-//            std::cout << "File is not open" << std::endl;
-//        }
-//    } else if (choice == "smelly") {
-//        inputFile.open("smelly.txt");
-//        if (inputFile.is_open()) {
-//            std::cout << "File is open\n";
-//        } else {
-//            std::cout << "File is not open" << std::endl;
-//        }
-//    } else {
-//        std::cout << "Invalid input" << std::endl;
-//    }
-//    return 0;
-//}
+
+
+
+int main1() {
+    std::ifstream inputFile("bee.txt");
+
+    std::ifstream replacedRulesSmelly("smelly_replacement.txt");
+    std::string choice, choiceReplace;
+    std::cin >> choice >> choiceReplace;
+    if (choice == "bee") {
+        inputFile.open("bee.txt");
+        if (inputFile.is_open()) {
+            std::string line;
+            std::getline(std::cin, line);
+            std::cout << line << std::endl;
+//            auto replacementRules{ifstream("bee_replacement.txt")};
+//            while (!replacementRules.eof()) {
+//                string line;
+//                getline(replacementRules, line);
+//                cout << line << endl;
+
+            std::cout << "File is open\n";
+        } else {
+            std::cout << "File is not open" << std::endl;
+        }
+    } else if (choice == "smelly") {
+        inputFile.open("smelly.txt");
+        if (inputFile.is_open()) {
+            std::cout << "File is open\n";
+        } else {
+            std::cout << "File is not open" << std::endl;
+        }
+    } else {
+        std::cout << "Invalid input" << std::endl;
+    }
+    return 0;
+}
 
 
