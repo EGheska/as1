258ff9f05fb056cbd04e2cf42389de2d6f74b0c6
Dmytro Taras <48064427+EGheska@users.noreply.github.com>
2023-02-24 13:53:59 +0100

    finally handled smth

--- main.cpp
+++ main.cpp
@@ -1,34 +1,53 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-auto findIndex(std::string toReplace, std::string replaced) {
-    std::ifstream replaceFile("bee_replacement.txt");
-    std::string buffer;
-    while (std::getline(replaceFile, buffer)) {
-        auto index = buffer.find("|");
-        toReplace = buffer.substr(0, index);
-        replaced = buffer.substr(index + 2, buffer.length());
-    }
-        return toReplace, replaced;
-}
+#include <vector>
+//auto findIndex(std::string toReplace, std::string replaced) {
+//    std::ifstream replaceFile("bee_replacement.txt");
+//    std::string buffer;
+//    while (std::getline(replaceFile, buffer)) {
+//        auto index = buffer.find("|");
+//        for (int i=0;i<buffer.length();i++){
+//            toReplace = buffer.substr(0, index);
+//            replaced = buffer.substr(index + 2, buffer.length());
+//
+//        }
+//
+//    }
+//    for (int i =0;i<toBeReplaced.size();i++){
+//        std::cout <<
+//    }
+//        return toReplace, replaced;
+//}
 int main(){
     std::ifstream beeFile("bee.txt");
     std::ifstream smellyFile("smelly.txt");
-    std::ofstream beeOut("bee.replaced");
+    std::ifstream replacedFile("bee_replacement.txt");
+    std::string line;
+    std::string replacedLine;
     std::string buffer;
+    std::string replacementBuffer;
     std::string choice, choiceReplace;
     std::cin >> choice >> choiceReplace;
     std::string toReplace, replaced;
     if (choice == "bee") {
-        findIndex(toReplace, replaced);
-        while(std::getline(beeFile,buffer)) {
-        if (buffer.find(toReplace) != std::string::npos) {
-                buffer.replace(buffer.find(toReplace), toReplace.length(), replaced);
+        while(std::getline(beeFile,line)) {
+            buffer += line + "\n";
         }
-            beeOut << buffer << "\n";
-            std::cout << buffer << "\n";
+        std::cout << buffer;
+        while(std::getline(replacedFile, replacementBuffer)){
+           auto index = replacementBuffer.find(" | ");
+           auto toReplace = replacementBuffer.substr(0, index);
+           auto onWhatReplace = replacementBuffer.substr(index+3);
+            while (true) {
+                try {
+                    buffer.replace(buffer.find(toReplace), toReplace.length(), onWhatReplace);
+                } catch(std::out_of_range&) {
+                    break;
+                }
+           }
         }
-
+        std::cout << buffer;
     } else if (choice == "smelly") {
         while(std::getline(smellyFile, buffer)){
             std::cout << buffer << "\n";
