6ce0f7d226675e17b46d7f7d46a03f96b40d50b2
Dmytro Taras <48064427+EGheska@users.noreply.github.com>
2023-03-06 14:21:08 +0100

    Smelly and Bee now working

--- main.cpp
+++ main.cpp
@@ -2,27 +2,12 @@
 #include <fstream>
 #include <string>
 #include <vector>
-//auto findIndex(std::string toReplace, std::string replaced) {
-//    std::ifstream replaceFile("bee_replacement.txt");
-//    std::string buffer;
-//    while (std::getline(replaceFile, buffer)) {
-//        auto index = buffer.find("|");
-//        for (int i=0;i<buffer.length();i++){
-//            toReplace = buffer.substr(0, index);
-//            replaced = buffer.substr(index + 2, buffer.length());
-//
-//        }
-//
-//    }
-//    for (int i =0;i<toBeReplaced.size();i++){
-//        std::cout <<
-//    }
-//        return toReplace, replaced;
-//}
+
 int main(){
     std::ifstream beeFile("bee.txt");
     std::ifstream smellyFile("smelly.txt");
     std::ifstream replacedFile("bee_replacement.txt");
+    std::ifstream replacedSmelly("smelly_replacement.txt");
     std::string line;
     std::string replacedLine;
     std::string buffer;
@@ -34,23 +19,51 @@ int main(){
         while(std::getline(beeFile,line)) {
             buffer += line + "\n";
         }
-        std::cout << buffer;
-        while(std::getline(replacedFile, replacementBuffer)){
-           auto index = replacementBuffer.find(" | ");
-           auto toReplace = replacementBuffer.substr(0, index);
-           auto onWhatReplace = replacementBuffer.substr(index+3);
-            while (true) {
-                try {
-                    buffer.replace(buffer.find(toReplace), toReplace.length(), onWhatReplace);
-                } catch(std::out_of_range&) {
-                    break;
+        if (choiceReplace == "bee_replacement") {
+
+
+            std::cout << "Original text: " << "\n";
+            std::cout << buffer;
+            while (std::getline(replacedFile, replacementBuffer)) {
+                auto index = replacementBuffer.find(" | ");
+                auto toReplace = replacementBuffer.substr(0, index);
+                auto onWhatReplace = replacementBuffer.substr(index + 3);
+                while (true) {
+                    try {
+                        buffer.replace(buffer.find(toReplace), toReplace.length(), onWhatReplace);
+                    } catch (std::out_of_range &) {
+                        break;
+                    }
                 }
-           }
+            }
+            std::cout << "Replaced text: " << "\n";
+            std::cout << buffer;
+        }else {
+            std::cerr << "Invalid name of the replaced file. Cannot find a file named " << choiceReplace;
         }
-        std::cout << buffer;
     } else if (choice == "smelly") {
-        while(std::getline(smellyFile, buffer)){
+        while(std::getline(smellyFile, line)){
+            buffer += line + "\n";
+        }
+        if (choiceReplace == "smelly_replaced"){
+            std::cout << "Original text: " << "\n";
             std::cout << buffer << "\n";
+            while(std::getline(replacedSmelly, replacementBuffer)){
+                auto index = replacementBuffer.find(" | ");
+                auto toReplace = replacementBuffer.substr(0,index);
+                auto onWhatReplace = replacementBuffer.substr(index +3);
+                while(true){
+                    try {
+                        buffer.replace(buffer.find(toReplace), toReplace.length(), onWhatReplace);
+                    } catch (std::out_of_range&){
+                        break;
+                    }
+                }
+            }
+            std::cout << "Replaced text: " << "\n";
+            std::cout << buffer + "\n";
+        } else {
+            std::cerr << "Invalid name of the replaced file. Cannot find a file named " << choiceReplace;
         }
     } else {
         std::cerr << "Invalid input. Cannot find a file named " << choice;
